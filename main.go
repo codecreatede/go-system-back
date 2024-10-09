51e7e9926d3508a75704e9cd0a198c9d543d6e30
Gaurav Sablok <gauravcodepro@gmail.com>
2024-10-09 15:38:07 +0200

    codecreatede
    second version finished coding and now have to implement the additional and enroute files and then a complete application to be deployed anywhere

--- main.go
+++ main.go
@@ -27,29 +27,165 @@ import (
 func main() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Fatal(err)
-		os.Exit(1)
 	}
+	os.Exit(1)
 }
 
 var (
-	path        string
-	destination string
+	hostpath        string
+	hostdestination string
+	exclude         string
+	include         string
+	foriegnaddress  string
+	foriegnpath     string
+	deletedir       bool
+	backupdir       bool
 )
 
 var rootCmd = &cobra.Command{
+	Use:  "option",
+	Long: "system backup application and uses cp, dd and rsync for the system back from local to the remote application development",
+}
+
+var cpCmd = &cobra.Command{
 	Use:  "system back",
 	Long: "This is the system back up configuration and it uses three main type, dd, cp and rsync",
 	Run:  systemBack,
 }
 
-func init() {
-	rootCmd.Flags().
-		StringVarP(&path, "path to the local file", "p", "path to the file which needs to be backed up", "system init path")
-	rootCmd.Flags().
-		StringVarP(&destination, "destination path", "d", "input the destination path", "system init destination path")
+var rsyncCmd = &cobra.Command{
+	Use:  "ryncdir",
+	Long: "recursive syncing of the directories on the host system",
+	Run:  rsyncHost,
 }
 
+var rsyncArchCmd = &cobra.Command{
+	Use:  "rsyncArch",
+	Long: "archiving of the system directories",
+	Run:  rsyncArch,
+}
+
+var rsyncHostPushCmd = &cobra.Command{
+	Use:  "rynscHostpull",
+	Long: "syncing the transfer files from the host to remote",
+	Run:  hostPush,
+}
+
+var rsyncHostPullCmd = &cobra.Command{
+	Use:  "rsyncHostpull",
+	Long: "syncing of the transfer file from the remote to the host",
+	Run:  hostPull,
+}
+
+var rsyncHostRemoteCmd = &cobra.Command{
+	Use:  "rsyncBack",
+	Long: "end to end syncing of the rsync between host and remote",
+	Run:  rsyncEnd,
+}
+
+var tunnelCmd = &cobra.Command{
+	Use:  "tunnel",
+	Long: "this is establishing a tunneling system and is equivalent to rsync -azP",
+	Run:  tunnelFunc,
+}
+
+var tunnelDCmd = &cobra.Command{
+	Use:  "tunnelD",
+	Long: "establishing a delete tunnel and this is rsync -an --delete",
+	Run:  tunnelDFunc,
+}
+
+func init() {
+	cpCmd.Flags().
+		StringVarP(&hostpath, "path to the file/folder", "p", "path to the file/folder which needs to be backed up", "system init path")
+	cpCmd.Flags().
+		StringVarP(&hostdestination, "destination path", "d", "input the destination path", "system init destination path")
+	rsyncCmd.Flags().
+		StringVarP(&hostpath, "path to the file/folder", "p", "path to the file/folder which needs to be backed up", "system init path")
+	rsyncCmd.Flags().
+		StringVarP(&hostdestination, "destination path", "d", "input the destination path", "system init destination path")
+	rsyncArchCmd.Flags().
+		StringVarP(&hostpath, "path to the file/folder", "p", "path to the file/folder which needs to be backed up", "system init path")
+	rsyncArchCmd.Flags().
+		StringVarP(&hostdestination, "destination path", "d", "input the destination path", "system init destination path")
+	rsyncHostPushCmd.Flags().StringVarP(&hostpath, "hostpath", "H", "path on the host", "host path")
+	rsyncHostPushCmd.Flags().
+		StringVarP(&foriegnpath, "path on the destination", "D", "destination path", "define path")
+	rsyncHostPushCmd.Flags().
+		StringVarP(&exclude, "anyfiles to exclude", "e", "exclusive", "exclude")
+	rsyncHostPushCmd.Flags().
+		StringVarP(&include, "include these files", "I", "inclusive", "include")
+	rsyncHostPushCmd.Flags().
+		StringVarP(&foriegnaddress, "address of the server", "A", "server address", "ip route")
+	rsyncHostPullCmd.Flags().StringVarP(&hostpath, "hostpath", "H", "path on the host", "host path")
+	rsyncHostPullCmd.Flags().
+		StringVarP(&foriegnpath, "path on the destination", "D", "destination path", "path on the remote")
+	rsyncHostPullCmd.Flags().
+		StringVarP(&exclude, "anyfiles to exclude", "e", "exclusive", "exclude")
+	rsyncHostPullCmd.Flags().
+		StringVarP(&include, "include these files", "I", "inclusive", "include")
+	rsyncHostPullCmd.Flags().
+		StringVarP(&foriegnaddress, "address of the server", "A", "server address", "ip route")
+
+	rootCmd.AddCommand(cpCmd)
+	rootCmd.AddCommand(rsyncCmd)
+	rootCmd.AddCommand(rsyncArchCmd)
+	rootCmd.AddCommand(rsyncHostPullCmd)
+	rootCmd.AddCommand(rsyncHostPushCmd)
+	rootCmd.AddCommand(rsyncHostRemoteCmd)
+	rootCmd.AddCommand(tunnelCmd)
+	rootCmd.AddCommand(tunnelDCmd)
+}
+
+/*
+   the time function and the embedded struct and the iter will be called in each function and will evaluate
+	 whether tot ake back up or not and this is reading directly from the remote server or the host server.
+
+*/
+
 func systemBack(cmd *cobra.Command, args []string) {
+	cpout, err := exec.Command("cp", hostpath, hostdestination).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(cpout))
+}
+
+func rsyncHost(cmd *cobra.Command, args []string) {
+	rsyncerr, err := exec.Command("rsync", "-r", hostpath, hostdestination).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(rsyncerr))
+}
+
+func rsyncArch(cmd *cobra.Command, args []string) {
+	rsyncArchA, err := exec.Command("rsync", "-az", hostpath, hostdestination).Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(rsyncArchA))
+}
+
+func hostPush(cmd *cobra.Command, args []string) {
+	hostpushAdd, err := exec.Command("rsync", "-a", hostpath, foriegnaddress, ":", foriegnpath).
+		Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(hostpushAdd))
+}
+
+func hostPull(cmd *cobra.Command, args []string) {
+	hostpullAdd, err := exec.Command("rsync", "-a", foriegnaddress, ":", foriegnpath, hostdestination).
+		Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(hostpullAdd))
+}
+
+func timeCheck(cmd *cobra.Command, args []string) {
 	time := time.Now()
 	writetime := time.String()
 	file, err := os.Create("currenttimefile.txt")
@@ -64,7 +200,6 @@ func systemBack(cmd *cobra.Command, args []string) {
 		log.Fatal(err)
 	}
 	fmt.Println("system date and time stage has been moved %s", move)
-
 	type timeNow struct {
 		yearNow     string
 		dateNow     string
@@ -95,7 +230,9 @@ func systemBack(cmd *cobra.Command, args []string) {
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	fRead := bufio.NewScanner(fOpen)
+
 	for fRead.Scan() {
 		line := fRead.Text()
 		timeStore = append(timeStore, timeDate{
